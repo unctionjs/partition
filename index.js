db4c9bea66e57f9cbb8bbf1bc8eccadfda099a11
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-05-26 20:17:29 -0700

    Removing packages we don't need

--- index.js
+++ index.js
@@ -1,25 +1,20 @@
-import append from "@unction/append"
-import couple from "@unction/couple"
-import fresh from "@unction/fresh"
-import reduceValues from "@unction/reducevalues"
-
-export default function partition (predicate: PredicateFunctionType): Function {
-  return function partitionPredicate (functor: FunctorType): [FunctorType, FunctorType] {
-    const freshFunctor = fresh(functor)
-    const initalFunctorPair = [
-      freshFunctor,
-      freshFunctor,
-    ]
-
-    return reduceValues((accumulation: [FunctorType, FunctorType]): Function => (value: ValueType): [FunctorType, FunctorType] => {
-      const [consequent, alternate] = accumulation
-      const appendedValue = append(value)
+import append from "@unction/append";
+import couple from "@unction/couple";
+import fresh from "@unction/fresh";
+import reduceValues from "@unction/reducevalues";
+export default function partition(predicate) {
+  return function partitionPredicate(functor) {
+    const freshFunctor = fresh(functor);
+    const initalFunctorPair = [freshFunctor, freshFunctor];
+    return reduceValues(accumulation => value => {
+      const [consequent, alternate] = accumulation;
+      const appendedValue = append(value);
 
       if (predicate(value)) {
-        return couple(appendedValue(consequent))(alternate)
+        return couple(appendedValue(consequent))(alternate);
       }
 
-      return couple(consequent)(appendedValue(alternate))
-    })(initalFunctorPair)(functor)
-  }
+      return couple(consequent)(appendedValue(alternate));
+    })(initalFunctorPair)(functor);
+  };
 }
