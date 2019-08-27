611bbf54c245c953e0489705b373ad0b28ab3a7a
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-08-27 02:06:21 -0700

    Switching to typescript

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
-/* eslint-disable flowtype/require-return-type */
+
 import {test} from "tap";
 
-import partition from "./index";
+import partition from "./";
 
 const isOdd = (value) => value % 2 != 0;
 
