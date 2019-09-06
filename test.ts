1535c1d4fd42588f2069b04ffcaadbe90f3c6063
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-09-06 14:56:49 -0700

    The transformer didnt fix this

--- test.ts
+++ test.ts
@@ -3,6 +3,6 @@ import partition from "./index";
 
 const isOdd = (value) => value % 2 != 0;
 
-test(() => {
+test("works", () => {
   expect(partition(isOdd)([1, 2, 3, 4])).toEqual([[1, 3], [2, 4]]);
 });
